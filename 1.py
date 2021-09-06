583e8f55ab008f8f411d750b56570e7cc9f7f48d
pulkit <pulkitmadaan.ameotech@gmail.com>
2021-09-06 18:59:24 +0530

    pythn

---
+++ 1.py
@@ -0,0 +1,258 @@
+# a="ababb"
+
+# for i in range(0,5):
+    # print(a[i:])
+
+# def suffix(s):
+#     if (len(s) == 0):
+#         print('lenth is 0')
+#         print(s)
+        
+#         return
+#     print(s)
+#     suffix(s[1:])
+    # print(s)
+# suffix("abcde")
+
+# a="bababacdef"
+# for i in range(0,10):
+#     print(a[i:])
+
+
+# a="baabaa"
+# for i in range(0,6):
+#     print(a[::-1])
+
+
+# def reverse(b):
+#     print(b[::-1])
+
+# b="baabaa"
+# reverse(b)
+# b="abbaabc"
+# def x(b):
+#     a=""
+
+#     for i in range((len(b)-1),-1,-1):
+#         a=a+(b[i])
+#     print(a)
+       
+ 
+# x(b)
+# a=[2,4,6]
+# b=[3,4,6]
+# i=0
+# j=0
+# while(i<len(a)):
+#     print(a[i]+b[i])
+#     i+=1
+    # j+=1
+
+# a=[2,4,6]
+# b=[3,4,6]
+# for i in a:
+#     for j in b:
+#         print(i*j)
+
+
+
+# a="baad"
+# b=""
+# for i in range(len(a)-1,-1,-1):
+#     b=b+(a[i])
+# print(b)
+# summ=0
+# a=[2,4,6,8]
+# for i in range(len(a)):
+#     if(i==2 or i==3):
+#         summ= summ+a[i]
+# if summ%2==0:
+#             a.append(summ)
+# print(a)
+# x={}
+# a=[1,2,2,2,5,10,10]
+# for i in a:
+#     if i in x.keys():
+#         x[i]+=1
+#     else:
+#         x[i]=1
+
+# print(x[5])
+# x={}
+# b=['aa','bb','aa','a','d']
+# for i in b:
+#     if i in x.keys():
+#         x[i]+=1
+#     else:
+#         x[i]=1
+# print(x)
+
+a=[2,3,5,6]
+for i in range(len(a)):
+    if(i==1):
+        b=a[i]*100
+        print(b)
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
